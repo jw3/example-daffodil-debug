17902038743ae18a2d7d6720f892e9f9cce7e6e5
John Wass <wassj@ctc.com>
2021-04-16 06:37:23 -0400

    auto step mode

--- src/main/scala/ddb/debugger/z/gui.scala
+++ src/main/scala/ddb/debugger/z/gui.scala
@@ -4,11 +4,11 @@ import ddb.debugger.api.{ControlProvider, Step}
 import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.scene.Scene
-import scalafx.scene.control.Button
+import scalafx.scene.control.{Button, ToggleButton}
 import scalafx.scene.image.{Image, ImageView}
 import scalafx.scene.paint.Color._
-import scalafx.scene.shape.Rectangle
-import zio.IO
+import zio.duration.durationInt
+import zio.{IO, Schedule}
 
 object gui {
   def run(cq: CProducer, myctls: ControlProvider*) =
@@ -23,7 +23,7 @@ object gui {
             height = 450
             scene = new Scene {
               fill = LightGreen
-              val button = new Button {
+              val step = new Button {
                 id = "debug-step"
                 layoutX = 0
                 layoutY = 350
@@ -31,13 +31,38 @@ object gui {
                 prefHeight = 48
                 graphic = new ImageView(new Image(this, s"/icons/debug-step-into.png"))
                 tooltip = "Step"
-                onMouseClicked = _ =>
+                onMouseClicked = _ => {
+                  step10.selected = false
                   zio.Runtime.default.unsafeRunAsync_(
                     cq.offer(Step.default)
                   )
+                }
               }
 
-              content = myctls.map(_.control) :+ button
+              val step10 = new ToggleButton {
+                id = "debug-play"
+                layoutX = 70
+                layoutY = 350
+                prefWidth = 48
+                prefHeight = 48
+                graphic = new ImageView(new Image(this, s"/icons/debug-run.png"))
+                tooltip = "Auto Step"
+
+                onAction = e => {
+                  zio.Runtime.default.unsafeRunAsync_(
+                    cq.offer(Step.default).delay(100.millis).repeatWhile(_ => selected.value)
+                  )
+                }
+              }
+
+              val jpg = new ImageView {
+                layoutX = 200
+                layoutY = 320
+
+                image = new Image(s"/works.jpg", 96,  96, true, true)
+              }
+
+              content = myctls.map(_.control) :+ step :+ step10 :+ jpg
             }
           }
         }
