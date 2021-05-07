3762d9a3e8f0b3607f89f6fad12da003eec7dccf
Adam Rosien <adam@rosien.net>
2021-05-07 12:05:03 -0700

    Don't print inscrutible response body as string.

--- src/main/scala/ddb/debugger/dapodil/logging.scala
+++ src/main/scala/ddb/debugger/dapodil/logging.scala
@@ -9,7 +9,7 @@ object logging {
     request => s"#${request.seq} ${request.command} ${request.arguments}"
 
   implicit val responseShow: Show[Response] =
-    response => s"#${response.request_seq} ${response.command} ${response.body}"
+    response => s"#${response.request_seq} ${response.command}"
 
   implicit val eventShow: Show[DebugEvent] =
     event => s"${event}"
