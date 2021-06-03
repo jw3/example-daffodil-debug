6b31256c8ae368ac454254d866821b6b24456d72
Adam Rosien <adam@rosien.net>
2021-06-03 14:05:58 -0700

    Log response bodies as JSON.

--- src/main/scala/ddb/debugger/dapodil/logging.scala
+++ src/main/scala/ddb/debugger/dapodil/logging.scala
@@ -1,22 +1,17 @@
 package ddb.debugger.dapodil
 
 import cats.Show
+import com.microsoft.java.debug.core.protocol._
 import com.microsoft.java.debug.core.protocol.Messages._
-import com.microsoft.java.debug.core.protocol.Events
 import com.microsoft.java.debug.core.protocol.Events._
-import com.microsoft.java.debug.core.protocol.Responses.InitializeResponseBody
-import com.microsoft.java.debug.core.protocol.JsonUtils
 
 object logging {
   implicit val requestShow: Show[Request] =
     request => s"#${request.seq} ${request.command} ${request.arguments}"
 
+  // Note/warning: all response bodies *should* be translatable to JSON, so we decode them for logging.
   implicit val responseShow: Show[Response] =
-    response =>
-      response.body match {
-        case body: InitializeResponseBody => s"#${response.request_seq} ${response.command} ${JsonUtils.toJson(body)}"
-        case _                            => s"#${response.request_seq} ${response.command}"
-      }
+    response => s"#${response.request_seq} ${response.command} ${JsonUtils.toJson(response.body)}"
 
   implicit val eventShow: Show[DebugEvent] = {
     case event: Events.StoppedEvent => s"${event.`type`} ${event.reason}"
